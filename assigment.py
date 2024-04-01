a63ff6fc3206ef5885c031cb9f4b1711196d2b49
raghavxope98 <165598529+raghavxope98@users.noreply.github.com>
2024-04-01 14:04:24 +0530

    Update assigment.py

--- assigment.py
+++ assigment.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
+import json
 
 
 def wait_for_product_elements():
@@ -25,8 +26,10 @@ def getProductDetails(allProducts, rating="4.0"):
                result.update({product: allProducts[product]})
     return result
 
+
 def wait_for_element_present(locator):
     return WebDriverWait(driver, 10).until(EC.presence_of_element_located(locator))
+
 
 def amazonLogin(driver,email,password):
     email_field = driver.find_element(By.ID, "ap_email")
@@ -36,8 +39,12 @@ def amazonLogin(driver,email,password):
     password_field.send_keys(password)
     password_field.send_keys(Keys.RETURN)
 
-def addingToCart(products):
+
+def addingToCart(products,productCount=1):
     for product in products.keys():
+        if productCount==0:
+            print("Finished adding products to the cart as per user mentioned count")
+            break
         productLink = products[product]["product_link"]
         driver.get(productLink)
         print("Adding {} successfully to cart".format(product))
@@ -47,26 +54,46 @@ def addingToCart(products):
             driver.find_element(By.ID, "attach-close_sideSheet-link")
         except:
             print(" product {} was not able to add to the cart".format(product))
+        productCount -=1
     verifyCart(product)
+
 
 def addingToWishlist(products):
         for product in products.keys():
             productLink = products[product]["product_link"]
             driver.get(productLink)
             driver.find_element(By.LINK_TEXT, "Add to Wish List").click()
-            amazonLogin(driver=driver, email='7899632046', password="Moremoney@8055")
+            amazonLogin(driver=driver, email=username, password=password)
 
 
 def verifyCart(products):
     driver.get("https://www.amazon.in/gp/cart/view.html")
     WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".sc-list-item-content")))
-    product_titles = driver.find_elements(By.CSS_SELECTOR, ".sc-list-item-content .aok-offscreen")
-    for title in product_titles:
-        print(title.text)
+    # product_titles = driver.find_elements(By.CSS_SELECTOR, ".sc-list-item-content .aok-offscreen")
+    # for title in product_titles:
+    #     print(title.text)
 
+
+def readInputValues(filename):
+    with open(filename, 'r') as file:
+        config = json.load(file)
+    return config
+
+#Reading all values from json file
+inputData = readInputValues('config.json')
+username = inputData['username']
+password = inputData['password']
+searchEngineURL = inputData['searchEngineURL']
+noPagesToSearch = inputData['noPagesToSearch']
+productCount = inputData['productCount']
+productName = inputData['productName']
+productCategory = inputData['productFilter']['productCategory']
+minPrice = inputData['productFilter']['minPrice']
+maxPrice = inputData['productFilter']['maxPrice']
+ratings = inputData['productFilter']['ratings']
 
 driver = webdriver.Chrome()
-driver.get("http://www.google.com")
+driver.get(searchEngineURL)
 search_box = driver.find_element(By.NAME, "q")
 search_box.send_keys("amazon")
 search_box.send_keys(Keys.RETURN)
@@ -77,18 +104,18 @@ for result in search_results:
 amazon_link = driver.find_element(By.PARTIAL_LINK_TEXT, "Amazon")
 amazon_link.click()
 dropdown = Select(driver.find_element(By.ID, "searchDropdownBox"))
-dropdown.select_by_visible_text("Electronics")
+dropdown.select_by_visible_text(productCategory)
 search_box = driver.find_element(By.ID, "twotabsearchtextbox")
-search_box.send_keys("dell")
+search_box.send_keys(productName)
 search_box.send_keys(Keys.RETURN)
 price_filter_low = driver.find_element(By.ID, "low-price")
-price_filter_low.send_keys("30000")
+price_filter_low.send_keys(minPrice)
 price_filter_high = driver.find_element(By.ID, "high-price")
-price_filter_high.send_keys("50000")
+price_filter_high.send_keys(maxPrice)
 driver.find_element(By.XPATH, "//input[@class='a-button-input']").click()
 product_elements = driver.find_elements(By.CSS_SELECTOR, "[data-component-type='s-search-result']")
 product_details = {}
-for page in range(2):
+for page in range(noPagesToSearch):
     print("Getting details of page {}".format(page+1))
     for product in product_elements:
         title_element = product.find_element(By.CSS_SELECTOR, ".a-text-normal")
@@ -108,7 +135,7 @@ for page in range(2):
         except NoSuchElementException:
             price = "Price not available"
         product_details.update({title: {"price": price, "rating": rating, "product_link": product_link}})
-    if page != 1:
+    if page != noPagesToSearch-1:
         next_button = driver.find_element(By.LINK_TEXT, "Next")
         next_button.click()
         if not wait_for_product_elements():
@@ -117,7 +144,7 @@ for page in range(2):
         product_elements = driver.find_elements(By.CSS_SELECTOR, "[data-component-type='s-search-result']")
 
 print(product_details)
-cartItems = getProductDetails(product_details, rating="3.8")
+cartItems = getProductDetails(product_details, rating=ratings)
 if cartItems is not None:
-    addingToCart(cartItems)
+    addingToCart(cartItems,productCount)
 driver.quit()
